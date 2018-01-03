070f3ed74902b007217b1796dd43209278b3ce44
Alberto Morales <kodenix@gmail.com>
2018-01-03 14:31:49 +0100

    Agregando StoreDevTool para visualizar los estados de la app. Se ha validado para usarlo solamente si estamos en entornos diferentes a produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { AppReducer } from './../redux/app.reducer';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { ResetComponent } from './reset/reset.component';
 
+import { environment } from '../environments/environment';
+
+// import dev only modules
+let developModules = [
+  StoreDevtoolsModule.instrument({
+    maxAge: 25
+  }),
+];
+
+// if production clear dev imports and set to prod mode
+if (environment.production === true) {
+  developModules = [];
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +30,8 @@ import { ResetComponent } from './reset/reset.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(AppReducer)
+    StoreModule.forRoot(AppReducer),
+    developModules
   ],
   providers: [],
   bootstrap: [AppComponent]
